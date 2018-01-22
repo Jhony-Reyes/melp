e184ef74aa42efbc287bdc53efcca7fe419658fb
Jhony Reyes <jhonysr19@gmail.com>
2018-01-22 03:48:33 -0600

    Add Facebook Buttons and package.json v.0.0.2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 +# melp
 +Melp aplication
++ Create with Inferno Create App.
+Run npm install to install all dependencies.
+Run npm start to start the development mode.
+Run npm run build to compile the files for production mode.
--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Component from "inferno-component";
 import "./registerServiceWorker";
 // Import modules
-import axios from 'axios';
+import axios from "axios";
 import _ from "lodash";
+import { ModalContainer, ModalDialog } from "react-modal-dialog";
 
 class App extends Component {
   // eslint-disable-next-line
@@ -11,20 +12,30 @@ class App extends Component {
     this.state = {
       data: [],
       asc: false,
-      desc: false
+      desc: false,
+      alphaa: false,
+      alphad: false,
+      isShowingModal: false,
+      restaurant: "",
+      site: ""
     };
     this.orderAsc = this.orderAsc.bind(this);
     this.orderDesc = this.orderDesc.bind(this);
+    this.alphaAsc = this.alphaAsc.bind(this);
+    this.alphaDesc = this.alphaDesc.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
   componentDidMount() {
-    axios.get('https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json')
-    .then(response => {
-      let data = response.data;
-      this.setState({ data: data });
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    axios
+      .get("https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json")
+      .then(response => {
+        let data = response.data;
+        this.setState({ data: data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
   orderAsc() {
     this.setState({
@@ -40,99 +51,231 @@ class App extends Component {
     });
     console.log(this.state.asc);
   }
+  alphaAsc() {
+    this.setState({
+      data: _.orderBy(this.state.data, ["name"], ["asc"]),
+      alphaa: true
+    });
+    console.log(this.state.desc);
+  }
+  alphaDesc() {
+    this.setState({
+      data: _.orderBy(this.state.data, ["name"], ["desc"]),
+      alphad: true
+    });
+    console.log(this.state.asc);
+  }
+  handleClick(site, name) {
+    let description = `Restaurant: ${name}; visit your website ${site}`;
+    this.setState({
+      isShowingModal: true,
+      restaurant: name,
+      site: site
+    });
+    var metas = document.getElementsByTagName("meta");
+    for (var i = 0; i < metas.length; i++) {
+      if (metas[i].getAttribute("property") === "og:description") {
+        return metas[i].setAttribute("content", description);
+      }
+    }
+  }
+  handleClose() {
+    this.setState({ isShowingModal: false });
+  }
+
   render() {
     return (
       <div className="App">
         <header>
-          <nav class="navbar is-link" aria-label="main navigation">
-            <div class="navbar-brand">
-              <a class="navbar-item" href="https://bulma.io">
+          <nav className="navbar is-dark" aria-label="main navigation">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="https://bulma.io">
                 MELP LOGO
               </a>
-              <button class="button navbar-burger">
-                <span>as</span>
-                <span>sas</span>
-                <span>as</span>
-              </button>
             </div>
           </nav>
         </header>
-        <section class="hero is-light is-medium">
-          <div class="hero-body">
-            <div class="container">
-              <h1 class="title">MELP</h1>
-              <h2 class="subtitle">Know best restaurants in the city</h2>
+        <section className="hero is-medium">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">MELP</h1>
+              <h2 className="subtitle">Know best restaurants in the city</h2>
             </div>
           </div>
         </section>
         <section className="container">
-          <table class="table is-striped is-hoverable is-fullwidth">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>
-                  Rating
-                  <div class="control" style="display: inline-block;">
-                    <label class="radio">
-                      <input
-                        type="radio"
-                        name="foobar"
-                        checked={this.state.asc}
-                        onClick={this.orderAsc}
-                      />
-                      Asc
-                    </label>
-                    <label class="radio">
-                      <input
-                        type="radio"
-                        name="foobar"
-                        checked={this.state.desc}
-                        onClick={this.orderDesc}
-                      />
-                      Desc
-                    </label>
-                  </div>
-                </th>
-                <th>Address</th>
-                <th>Contact</th>
-              </tr>
-            </thead>
-            <tfoot>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Rating</th>
-                <th>Address</th>
-                <th>Contact</th>
-              </tr>
-            </tfoot>
-            <tbody>
-              {this.state.data.map(value => {
-                return (
-                  <tr>
-                    <th>{value.id}</th>
-                    <th>{value.name}</th>
-                    <th>{value.rating}</th>
-                    <th>
-                      State: {value.address.state} <br/>
-                      City: {value.address.city}<br/>
-                      Street: {value.address.street}<br/>
-                      Location: <br/>
-                      Lat: {value.address.location.lat} <br/>
-                      Lng: {value.address.location.lng}
-                    </th>
-                    <th>
-                      Site: <a href={value.contact.site}>{value.contact.site}</a> <br/>
-                      Email: <a href={`mailto:${value.contact.email}`}>{value.contact.email}</a> <br/>
-                      Phone: {value.contact.phone}
-                    </th>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          <div className="table">
+            <table class="table is-striped is-hoverable is-fullwidth">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>
+                    <div>Name</div>
+                    <div class="control table__rating">
+                      <label class="radio">
+                        <input
+                          type="radio"
+                          name="foobar"
+                          checked={this.state.alphaa}
+                          onClick={this.alphaAsc}
+                        />
+                        Asc
+                      </label>
+                      <label class="radio">
+                        <input
+                          type="radio"
+                          name="foobar"
+                          checked={this.state.alphad}
+                          onClick={this.alphaDesc}
+                        />
+                        Desc
+                      </label>
+                    </div>
+                  </th>
+                  <th>
+                    <div>Rating</div>
+                    <div class="control table__rating">
+                      <label class="radio">
+                        <input
+                          type="radio"
+                          name="foobar"
+                          checked={this.state.asc}
+                          onClick={this.orderAsc}
+                        />
+                        Asc
+                      </label>
+                      <label class="radio">
+                        <input
+                          type="radio"
+                          name="foobar"
+                          checked={this.state.desc}
+                          onClick={this.orderDesc}
+                        />
+                        Desc
+                      </label>
+                    </div>
+                  </th>
+                  <th>Address</th>
+                  <th>Contact</th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Rating</th>
+                  <th>Address</th>
+                  <th>Contact</th>
+                </tr>
+              </tfoot>
+              <tbody>
+                {this.state.data.map((value, idx) => {
+                  return (
+                    <tr>
+                      <th>
+                        <div className="table__id">{value.id}</div>
+                      </th>
+                      <th>{value.name}</th>
+                      <th>{value.rating}</th>
+                      <th>
+                        State:{" "}
+                        <span className="table__item">
+                          {value.address.state}{" "}
+                        </span>
+                        <br />
+                        City:{" "}
+                        <span className="table__item">
+                          {value.address.city}
+                        </span>
+                        <br />
+                        Street:{" "}
+                        <span className="table__item">
+                          {value.address.street}
+                        </span>
+                        <br />
+                        Location: <br />
+                        <span className="table__item">
+                          Lat: {value.address.location.lat}
+                        </span>{" "}
+                        <br />
+                        <span className="table__item">
+                          Lng: {value.address.location.lng}
+                        </span>{" "}
+                      </th>
+                      <th>
+                        Site:{" "}
+                        <a
+                          className="table__item"
+                          target="_blank"
+                          href={value.contact.site}
+                        >
+                          {value.contact.site}
+                        </a>{" "}
+                        <br />
+                        Email:{" "}
+                        <a
+                          className="table__item"
+                          href={`mailto:${value.contact.email}`}
+                        >
+                          {value.contact.email}
+                        </a>{" "}
+                        <br />
+                        Phone:{" "}
+                        <span className="table__item">
+                          {value.contact.phone}
+                        </span>{" "}
+                        <br />
+                        {
+                          <span
+                            onClick={e =>
+                              this.handleClick(
+                                value.contact.site,
+                                value.name,
+                                e
+                              )}
+                            className="table__fb"
+                          >
+                            <i
+                              className="fa fa-facebook-square"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        }
+                      </th>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </section>
+        {this.state.isShowingModal ? (
+          <ModalContainer onClose={this.handleClose}>
+            <ModalDialog onClose={this.handleClose}>
+              <h1 className="modal__title">
+                Restaurant: {this.state.restaurant}
+              </h1>
+              <h2>
+                Visit your website{" "}
+                <a target="_blank" href={this.state.site}>
+                  {this.state.site}
+                </a>
+              </h2>
+              <br />
+              <div style="text-align:center">
+                <iframe
+                  src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fjhony-reyes.github.io%2Fmelp%2F&width=143&layout=button&action=like&size=small&show_faces=false&share=true&height=65&appId"
+                  width="143"
+                  height="25"
+                  style="border:none;overflow:hidden"
+                  scrolling="no"
+                  frameborder="0"
+                  allowTransparency="true"
+                />
+              </div>
+            </ModalDialog>
+          </ModalContainer>
+        ) : null}
       </div>
     );
   }
